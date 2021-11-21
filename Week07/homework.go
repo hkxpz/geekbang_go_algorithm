d749ae07260f990fe9b8125026c107bbc5f88058
hkxpz <2446392171@qq.com>
2021-11-21 21:08:29 +0800

    第七周作业

---
+++ Week07/homework.go
@@ -0,0 +1,41 @@
+package Week07
+
+import "math"
+
+//279. 完全平方数
+func numSquares(n int) int {
+	f := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		minn := math.MaxInt32
+		for j := 1; j*j <= i; j++ {
+			minn = min(minn, f[i-j*j])
+		}
+		f[i] = minn + 1
+	}
+	return f[n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//279. 完全平方数
+func canJUmp(nums []int) bool {
+	if len(nums) <= 1 {
+		return true
+	}
+	dp := make([]bool, len(nums))
+	dp[0] = true
+	for i := 1; i < len(nums); i++ {
+		for j := i - 1; j >= 0; j-- {
+			if dp[j] && nums[j]+j >= i {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(nums)-1]
+}
